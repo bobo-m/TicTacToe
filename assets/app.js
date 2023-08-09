46b78323ee6d300f3915b76e6173dbecf93ced47
Bob Murmu <bobmurmu02@gmail.com>
2023-08-10 03:16:35 +0530

    Last working version

--- assets/app.js
+++ assets/app.js
@@ -1,5 +1,6 @@
-const choices= document.querySelector(".choices-toggle");
+const choices= document.querySelector(".choices-container");
 const vsPlayer = document.getElementById("vs-player");
+const vsCpu = document.getElementById("vs-cpu");
 const nextRound = document.querySelector('.next-round');
 //replacing layout
 const startGameSection = document.querySelector(".start-game");
@@ -7,17 +8,18 @@ const activeGameSection = document.querySelector(".game-active");
 const resultDisplay = document.getElementById('round-results');
 const restartGame = document.querySelector('.restart-game');
 let currentPlayer='x';
+const player1 = document.querySelector('.choices-toggle');
 //variables for game logic
 const cellContainer = document.querySelector('.cell-container');
 const cells = document.querySelectorAll(".cell");
-let gameState =['','','','','','','','',''];
+let gameState =[0,1,2,3,4,5,6,7,8];
 const winSequences=[
     [0,1,2]
    ,[3,4,5]
    ,[6,7,8]
    ,[0,3,6]
    ,[1,4,7]
-   ,[1,5,8]
+   ,[2,5,8]
    ,[0,4,8]
    ,[2,4,6]
 ];
@@ -28,96 +30,204 @@ let oScoreCount=0;
 const xScore=document.getElementById('x-score');
 const tiesScore=document.getElementById('ties-score');
 const oScore=document.getElementById('o-score');
-//  function getCurrentPlayer(event){
-//      const choiceX= choices.querySelector(".choices-x");
-//      const choiceO=choices.querySelector(".choices-o");
-//      const choiceImgX=choiceX.querySelector('.choices');
-//      const choiceImgO=choiceO.querySelector('.choices')
-//      if(event.target===choiceX){
-//          currentPlayer='x';
-//          choiceX.style.backgroundColor=('rgba(168,191,201,255)');
-//          choiceImgX.style.fill=('rgba(26,42,50,255)');
-//          choiceO.style.backgroundColor=('rgba(26,42,50,255)');
-//          choiceImgO.style.fill=('rgba(168,191,201,255)');
-//      }else{
-//          currentPlayer='o';
-//          choiceO.style.backgroundColor=('rgba(168,191,201,255)');
-//          choiceImgO.style.fill=('rgba(26,42,50,255)');
-//          choiceX.style.backgroundColor=('rgba(26,42,50,255)');
-//          choiceImgX.style.fill=('rgba(168,191,201,255)');
-//      }   
-//  }
-function startGame(){
+let p1;
+let huPlayer='x';
+let aiPlayer;
+function generateRandomMove(){
+    let randomIndex = Math.floor(Math.random()*8);
+    return randomIndex;
+}
+function getPlayer1(event){
+    const selected=event.target;
+    p1=selected.id;
+    let unselected;
+    p1==='x'?unselected=document.getElementById('o'):unselected=document.getElementById('x');
+    selected.classList.add('selected');
+    selected.classList.remove('unselected');
+    unselected.classList.add('unselected');
+    unselected.classList.remove('selected');
+    huPlayer=p1;
+}
+function startGame(mode){
     startGameSection.classList.add("hidden");
     activeGameSection.classList.remove("hidden");
     gameActive=true;
+    displayScore();
+    if(mode==='mplayer'){
+        cellContainer.addEventListener('click',handleCellClick);
+        huPlayer==='x';
+    }else if(mode='splayer'){
+        huPlayer==='x'? aiPlayer='o': aiPlayer='x';
+        if(huPlayer==='o'){
+            handleCpuFirstMove();
+        }
+        cellContainer.addEventListener('click',handleCpuGame);
+    }
 }
-function handleCellClick(event){
+function handleCpuFirstMove(){
+    let aiIndex=generateRandomMove();
+    cells[aiIndex].removeEventListener('mouseleave',cellHoverLeave);
+    cells[aiIndex].removeEventListener('mouseenter',cellHoverEnter);
+    cells[aiIndex].style.backgroundImage = ('url(assets/images/icon-x.svg)');
+    gameState[aiIndex]='x';
+}
+function handleCpuGame(event){
     event.target.removeEventListener('mouseleave',cellHoverLeave);
     event.target.removeEventListener('mouseenter',cellHoverEnter);
     const index = parseInt(event.target.getAttribute('cell-index'));
-    if(gameState[index]!==''|| !gameActive){
+    if(typeof gameState[index]!=='number'|| !gameActive){
         return;
     }
-    if(currentPlayer==='x'){
+    if(huPlayer==='x'){
         event.target.style.backgroundImage = ('url(assets/images/icon-x.svg)');
         gameState[index]='x';
+        handleResultValidation(gameState,'mplayer',huPlayer);
+        let bestMove=getCpuMove(gameState,aiPlayer);
+        let aiIndex=bestMove.index;
+        cells[aiIndex].style.backgroundImage = ('url(assets/images/icon-o.svg)');
+        cells[aiIndex].removeEventListener('mouseleave',cellHoverLeave);
+        cells[aiIndex].removeEventListener('mouseenter',cellHoverEnter);
+        gameState[aiIndex]='o';
+        handleResultValidation(gameState,'mplayer',aiPlayer);
     }else{
         event.target.style.backgroundImage = ('url(assets/images/icon-o.svg)');
         gameState[index]='o';
+        handleResultValidation(gameState,'mplayer',huPlayer);
+        let bestMove=getCpuMove(gameState,aiPlayer);
+        let aiIndex=bestMove.index;
+        cells[aiIndex].style.backgroundImage = ('url(assets/images/icon-x.svg)');
+        cells[aiIndex].removeEventListener('mouseleave',cellHoverLeave);
+        cells[aiIndex].removeEventListener('mouseenter',cellHoverEnter);
+        gameState[aiIndex]='x';
+        handleResultValidation(gameState,'mplayer',aiPlayer);
     }
-    handleResultValidation();
+    
+}
+function displayScore(){
+    const playerX=document.querySelector(".player-x");
+    const playerO=document.querySelector(".player-o");
+    if(p1===undefined || p1==='x'){
+        playerX.innerHTML=(`X (P1)`);
+        playerO.innerHTML=(`O (P2)`);
+    }else{
+        playerX.innerHTML=(`X (P2)`);
+        playerO.innerHTML=(`O (P1)`);
+    }
+}
+//minimax algo
+function getCpuMove(currentState,player){
+    const availSpots=getEmptyIndexes(currentState);
+    if(handleResultValidation(currentState,'splayer',huPlayer)){
+        return{score:-10};
+    }else if(handleResultValidation(currentState,'splayer',aiPlayer)){
+        return{score:10};
+    }else if(availSpots.length===0){
+        return{score:0};
+    }
+    let moves = [];
+    for(let i=0;i<availSpots.length;i++){
+        let move={};
+        move.index=availSpots[i];
+        currentState[availSpots[i]]=player;
+        if(player===aiPlayer){
+            let result=getCpuMove(currentState,huPlayer);
+            move.score=result.score;
+        }else{
+            let result=getCpuMove(currentState,aiPlayer);
+            move.score=result.score;
+        }
+        currentState[availSpots[i]]=move.index;
+        moves.push(move);
+    }    
+    let bestMove;
+        if(player===huPlayer){
+            let bestScore=1000;
+            for(let i=0;i<moves.length;i++){
+                if(moves[i].score<bestScore){
+                    bestScore=moves[i].score;
+                    bestMove=i;
+                }
+            }
+        }else{
+            {
+                let bestScore=-1000;
+                for(let i=0;i<moves.length;i++){
+                    if(moves[i].score>bestScore){
+                        bestScore=moves[i].score;
+                        bestMove=i;
+                    }
+                }
+            }
+        }
+    return moves[bestMove];
+}
+function getEmptyIndexes(currentState){
+    return currentState.filter(index=>typeof(index)==('number'));
+}
+function handleCellClick(event){
+    const cellIndex=event.target;
+    const index = parseInt(event.target.getAttribute('cell-index'));
+    if(typeof gameState[index]!=='number'|| !gameActive){
+        return;
+    }
+    placeMark(cellIndex,currentPlayer)
+    gameState[index]=currentPlayer;
+    handleResultValidation(gameState);
+    changeHoverClass(cellIndex,currentPlayer);
     handlePlayerChange();
 }
 function handlePlayerChange(){
     currentPlayer==='x' ? currentPlayer='o' :currentPlayer='x' ;
 }
-function handleResultValidation(){
+function handleResultValidation(currentState, mode='mplayer', player=currentPlayer){
     let roundWon=false;
     let wonSequence=[];
     for(const sequence of winSequences){
-        const a = gameState[sequence[0]];
-        const b = gameState[sequence[1]];
-        const c = gameState[sequence[2]];
-        if(a ==''||b ==''||c ==''){
+        const a = currentState[sequence[0]];
+        const b = currentState[sequence[1]];
+        const c = currentState[sequence[2]];
+        if(typeof(a) =='number'||typeof(b) =='number'||typeof(c) =='number'){
             continue;
         }
-        if(a==b && b==c){
+        if(a==b && b==c && c==player){
             roundWon=true;
+            if(mode==='splayer'){
+                return true;
+            }
             wonSequence=sequence;
             break;
         }
     }
+    if(!roundWon && mode==='splayer'){
+        return false;
+    }
     if(roundWon){
-        for(let i=0;i<3;i++){
-            if(currentPlayer==='x'){
-                cells[wonSequence[i]].style.backgroundImage = ('url(assets/images/icon-x-bg.svg)');
-                cells[wonSequence[i]].style.backgroundColor = ('rgba(49,195,189,255)');
-
-            }else{
-                cells[wonSequence[i]].style.backgroundImage = ('url(assets/images/icon-o-bg.svg)');
-                cells[wonSequence[i]].style.backgroundColor = ('rgba(249,177,56,255)');
-            }
-        }
-        handleResult(currentPlayer);
+        styleWinPattern(wonSequence);
+        handleResult(player);
         gameActive=false;
         return;
     }
-    let roundDraw= !gameState.includes("");
-    if(roundDraw){
+    let roundDraw= getEmptyIndexes(currentState);
+    if(roundDraw.length==0){
         gameActive=false;
         handleResult();
+    }
+}
+function styleWinPattern(sequence){
+    for(let i=0;i<3;i++){
+        cells[sequence[i]].classList.add(`win-sequence-${currentPlayer}`);
     }
 }
 function handleResult(result){
     const winner = document.querySelector('.winner');
     const winnerMessage = document.querySelector('.winner-message');
     resultDisplay.classList.remove('hidden');
+    
     while(winnerMessage.hasChildNodes()){
         winnerMessage.removeChild(winnerMessage.firstElementChild);
     }
     if (result==='x'){
-        winner.innerHTML='PLAYER 1 WINS!!';
+        winner.innerHTML=`PLAYER ${p1==='x'||p1===undefined?1:2} WINS!!`;
         const image=document.createElement('img');
         image.setAttribute('src','assets/images/icon-x.svg');
         winnerMessage.appendChild(image);
@@ -128,7 +238,7 @@ function handleResult(result){
         xScoreCount++;
         xScore.innerHTML=`${xScoreCount}`;
     }else if(result==='o'){
-        winner.innerHTML='PLAYER 2 WINS!!';
+        winner.innerHTML=`PLAYER ${p1==='o'?1:2} WINS!!`;
         const image=document.createElement('img');
         image.setAttribute('src','assets/images/icon-o.svg');
         winnerMessage.appendChild(image);
@@ -144,55 +254,57 @@ function handleResult(result){
         resultText.style.color=('rgba(168,191,201,255)');
         winnerMessage.appendChild(resultText);
         tiesScoreCount++;
-    tiesScore.innerHTML=`${tiesScoreCount}`;
+        tiesScore.innerHTML=`${tiesScoreCount}`;
     }
 }
-function handleNextRound(){
+
+function resetCells(){
     resultDisplay.classList.add('hidden');
     for(const cell of cells){
-        cell.style.backgroundImage=('none');
-        cell.style.backgroundColor=('rgba(31,54,65,255)');
-        cell.addEventListener('mouseenter',cellHoverEnter);
-        cell.addEventListener('mouseleave',cellHoverLeave);
+        cell.className = ('cell');
     }
+    cellContainer.className=('cell-container x-hover');
+}
+function handleNextRound(){
+    resetCells();
     currentPlayer='x';
-    gameState =['','','','','','','','',''];    
+    gameState =[0,1,2,3,4,5,6,7,8];    
     gameActive=true;
+    if(aiPlayer==='x'){
+        handleCpuFirstMove();
+    }
 }
 function handleRestart(){
-    gameState =['','','','','','','','','']; 
+    gameState =[0,1,2,3,4,5,6,7,8]; 
     gameActive=true;
     currentPlayer='x';
-    for(const cell of cells){
-        cell.style.backgroundImage=('none');
-        cell.style.backgroundColor=('rgba(31,54,65,255)');
-        cell.addEventListener('mouseenter',cellHoverEnter);
-        cell.addEventListener('mouseleave',cellHoverLeave);
-    }
+    resetCells();
     xScoreCount=0;
     tiesScoreCount=0;
     oScoreCount=0;
     xScore.innerHTML = 0;
     tiesScore.innerHTML = 0;
     oScore.innerHTML = 0;
-}
-//hover effect
-function cellHoverEnter(event){
-    if(currentPlayer==='x'){
-        event.target.style.backgroundImage = ('url(assets/images/icon-x-outline.svg)');
-    }else{
-        event.target.style.backgroundImage = ('url(assets/images/icon-o-outline.svg)');
+    if(aiPlayer==='x'){
+        handleCpuFirstMove();
     }
 }
-function cellHoverLeave(event){
-    event.target.style.backgroundImage = ('none');
+//hover effect
+function changeHoverClass(cellIndex,player){
+    cellContainer.classList.remove(`${player}-hover`);
+    cellContainer.classList.add(`${player==='x'?'o':'x'}-hover`);
+    cellIndex.classList.add('played');
 }
-for(const cell of cells){
-    cell.addEventListener('mouseenter',cellHoverEnter);
-    cell.addEventListener('mouseleave',cellHoverLeave);
+function placeMark(cellIndex,player){
+    cellIndex.classList.add(`${player}-mark`);
 }
 restartGame.addEventListener('click',handleRestart);
 nextRound.addEventListener('click',handleNextRound);
-cellContainer.addEventListener('click',handleCellClick);
-vsPlayer.addEventListener('click',startGame);
+vsPlayer.addEventListener('click',()=>{
+    startGame('mplayer');
+});
+vsCpu.addEventListener('click',()=>{
+    startGame('splayer');
+});
+player1.addEventListener('click',getPlayer1);
 // choices.addEventListener('click',getCurrentPlayer);
